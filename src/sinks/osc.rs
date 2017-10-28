310af98327e6ded0ca835e9771b8a6e63943114e
Dave Poulter <hello@davepoulter.net>
2017-10-28 21:47:24 +0100

    Fix clippy warnings in osc sink

--- src/sinks/osc.rs
+++ src/sinks/osc.rs
@@ -15,6 +15,45 @@ impl From<Error> for SysErr {
     }
 }
 
+fn dispatch(sock: &UdpSocket, msg: Command) {
+    match msg {
+        Command::MidiNoteOn(chn, pitch, vel) => {
+            let buff = encoder::encode(&OscPacket::Message(OscMessage {
+                addr: "/note_on".to_string(),
+                args: Some(vec![
+                    OscType::Int(i32::from(chn)),
+                    OscType::Int(i32::from(pitch)),
+                    OscType::Int(i32::from(vel)),
+                ]),
+            })).unwrap();
+
+            sock.send(&buff).unwrap();
+        }
+        Command::MidiNoteOff(chn, pitch) => {
+            let buff = encoder::encode(&OscPacket::Message(OscMessage {
+                addr: "/note_off".to_string(),
+                args: Some(vec![
+                    OscType::Int(i32::from(chn)),
+                    OscType::Int(i32::from(pitch)),
+                ]),
+            })).unwrap();
+            sock.send(&buff).unwrap();
+        }
+        Command::MidiCtl(chn, ctl, val) => {
+            let buff = encoder::encode(&OscPacket::Message(OscMessage {
+                addr: "/ctrl".to_string(),
+                args: Some(vec![
+                    OscType::Int(i32::from(chn)),
+                    OscType::Int(i32::from(ctl)),
+                    OscType::Int(i32::from(val)),
+                ]),
+            })).unwrap();
+            sock.send(&buff).unwrap();
+        }
+        _ => (),
+    }
+}
+
 pub struct Osc;
 
 impl Osc {
@@ -23,48 +62,9 @@ impl Osc {
                -> Result<Self, SysErr> {
         let sock = try!(UdpSocket::bind("127.0.0.1:34254"));
         try!(sock.connect("127.0.0.1:3000"));
-
         thread::spawn(move || while let Ok(msg) = channel.recv() {
-            match msg {
-                Command::MidiNoteOn(chn, pitch, vel) => {
-                    let buff =
-                        encoder::encode(&OscPacket::Message(OscMessage {
-                            addr: "/note_on".to_string(),
-                            args: Some(vec![
-                                OscType::Int(chn as i32),
-                                OscType::Int(pitch as i32),
-                                OscType::Int(vel as i32),
-                            ]),
-                        })).unwrap();
-                    sock.send(&buff).unwrap();
-                }
-                Command::MidiNoteOff(chn, pitch) => {
-                    let buff =
-                        encoder::encode(&OscPacket::Message(OscMessage {
-                            addr: "/note_off".to_string(),
-                            args: Some(vec![
-                                OscType::Int(chn as i32),
-                                OscType::Int(pitch as i32),
-                            ]),
-                        })).unwrap();
-                    sock.send(&buff).unwrap();
-                }
-                Command::MidiCtl(chn, ctl, val) => {
-                    let buff =
-                        encoder::encode(&OscPacket::Message(OscMessage {
-                            addr: "/ctrl".to_string(),
-                            args: Some(vec![
-                                OscType::Int(chn as i32),
-                                OscType::Int(ctl as i32),
-                                OscType::Int(val as i32),
-                            ]),
-                        })).unwrap();
-                    sock.send(&buff).unwrap();
-                }
-                _ => (),
-            }
+            dispatch(&sock, msg);
         });
-
         Ok(Osc {})
     }
 }
